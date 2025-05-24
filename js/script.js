3c8204bff0508a3baac2f0ae89c8d739f4d451e3
LuqmanNurH <140305455+cherrylnh17@users.noreply.github.com>
2025-05-24 18:42:17 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('sidebar').addEventListener('click', function(){
+    document.getElementById('absolutebg').classList.toggle('absolute-active');
+    document.getElementById('content').classList.toggle('active');
+    document.getElementById('wrap').classList.toggle('active');
+});
